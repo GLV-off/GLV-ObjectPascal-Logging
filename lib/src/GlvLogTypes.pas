9fbdb52738adc1b6d284a48c8df5407ba98cfb29
GLV_off <glv.off@mail.ru>
2026-08-10 12:53:21 +0300

    DOC: GlvLogTypes - comments, documentation + update reference doc checklist

--- lib/src/GlvLogTypes.pas
+++ lib/src/GlvLogTypes.pas
@@ -1,3 +1,9 @@
+{
+ Module:       GlvLogTypes
+ Author:       GLV_off
+ Description:  Basic types of logging library.
+ History:      2026-08-10: Created
+}
 unit GlvLogTypes;
 
 {$I 'glv_log_lib.inc'}
@@ -5,18 +11,42 @@ unit GlvLogTypes;
 interface
 
 type
+  {
+   Type declares level of logging wich
+   linked single message:
+
+   llInfo    Information
+   llDebug   Debug message
+   llError   Error message
+   llWarn    Warning message
+  }
   TLogLvl = (llInfo, llDebug, llError, llWarn);
 
+  {
+   Type helper for Log Level enumeration
+  }
   TLogLvlHelper = record helper for TLogLvl
   public
+    {
+     Convert enumeration value to string as
+     helper method
+    }
     function AsStr: UnicodeString;
   end;
 
+{
+ Converting enumeration value to string. Return value
+ will be full UPPERCASE one word string value without
+ enumeration prefixes.
+}
 function LogLvlAsStr(const ALvl: TLogLvl): UnicodeString;
 
 implementation
 
 function LogLvlAsStr(const ALvl: TLogLvl): UnicodeString;
+{ Converting enumeration value to string. Return value
+  will be full UPPERCASE one word string value without
+  enumeration prefixes. }
 begin
   case ALvl of
     llDebug: Result := 'DEBUG';
@@ -28,6 +58,8 @@ begin
 end;
 
 function TlogLvlHelper.AsStr: UnicodeString;
+{ Convert enumeration value to string as
+  helper method }
 begin
   Result := LogLvlAsStr(Self);
 end;
